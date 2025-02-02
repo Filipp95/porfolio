4c307c45a1afa39bc891fa8c05be9a4c34ca6c88
unknown <fnarayev@gmail.com>
2025-02-02 23:28:40 +0300

    Updates

--- src/components/elements/ImageComponent/ImageComponent.jsx
+++
@@ -1,8 +0,0 @@
-const ImageComponent = ({className, source, description}) => {
-    return (
-            <img className={className} src={source} alt={description} />
-    )
-}
-
-export default ImageComponent;
-
